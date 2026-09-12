0cc70e255a84484cff5930dfd213efdc0c521279
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-09-12 21:12:36 +0900

    真空から取り出したような値(out-of-shin-air values)

--- atomics-and-locks/src/main.rs
+++ atomics-and-locks/src/main.rs
@@ -14,25 +14,20 @@ use std::{
     thread::{self, Thread},
 };
 
+static X: AtomicI32 = AtomicI32::new(0);
+static Y: AtomicI32 = AtomicI32::new(0);
+
 fn main() {
-    let key: String = get_key().to_string();
-    println!("{key}")
-}
-
-fn get_key() -> u64 {
-    static KEY: AtomicU64 = AtomicU64::new(0);
-    let key = KEY.load(Relaxed);
-    if key == 0 {
-        let new_key = generate_random_key();
-        match KEY.compare_exchange(0, new_key, Relaxed, Relaxed) {
-            Ok(_) => new_key,
-            Err(k) => k,
-        }
-    } else {
-        key
-    }
-}
-
-fn generate_random_key() -> u64 {
-    1
+    let a = thread::spawn(|| {
+        let x = X.load(Relaxed);
+        Y.store(x, Relaxed);
+    });
+    let b = thread::spawn(|| {
+        let y = Y.load(Relaxed);
+        X.store(y, Relaxed);
+    });
+    a.join().unwrap();
+    b.join().unwrap();
+    assert_eq!(X.load(Relaxed), 0);
+    assert_eq!(Y.load(Relaxed), 0);
 }

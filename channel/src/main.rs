05dda88c619056e53a16b7277a57d9902402f811
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-09-13 21:43:02 +0900

    update

--- channel/src/main.rs
+++ channel/src/main.rs
@@ -15,32 +15,64 @@ use std::{
 };
 
 pub struct Channel<T> {
-    queue: Mutex<VecDeque<T>>,
-    item_ready: Condvar,
+    message: UnsafeCell<MaybeUninit<T>>,
+    in_use: AtomicBool,
+    ready: AtomicBool,
 }
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
 
 impl<T> Channel<T> {
     pub fn new() -> Self {
         Self {
-            queue: Mutex::new(VecDeque::new()),
-            item_ready: Condvar::new(),
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            in_use: AtomicBool::new(false),
+            ready: AtomicBool::new(false),
         }
     }
 
+    /// 2つ以上のメッセージを送信しようとしたらパニックする
     pub fn send(&self, message: T) {
-        self.queue.lock().unwrap().push_back(message);
-        self.item_ready.notify_one();
+        if self.in_use.swap(true, Relaxed) {
+            panic!("can't send more than one message!");
+        }
+        unsafe { (*self.message.get()).write(message) };
+        self.ready.store(true, Release);
     }
 
+    /// メッセージがなければパニックする
+    /// メッセージがすでに読み込まれていてもパニックする
     pub fn receive(&self) -> T {
-        let mut b = self.queue.lock().unwrap();
-        loop {
-            if let Some(message) = b.pop_front() {
-                return message;
-            }
-            b = self.item_ready.wait(b).unwrap();
+        if !self.ready.swap(false, Acquire) {
+            panic!("no message available");
+        }
+        unsafe { (*self.message.get()).assume_init_read() }
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.ready.load(Relaxed)
+    }
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
         }
     }
 }
 
-fn main() {}
+fn main() {
+    let channel = Channel::new();
+    let t = thread::current();
+    thread::scope(|s| {
+        s.spawn(|| {
+            channel.send("hello world!");
+            t.unpark();
+        });
+        while !channel.is_ready() {
+            thread::park();
+        }
+        assert_eq!(channel.receive(), "hello world!")
+    });
+}

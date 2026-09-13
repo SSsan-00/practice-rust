93388450a1acd36ce3e38b6ebcdd1f34d17adffb
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-09-13 22:26:10 +0900

    別バージョンのチャネル実装

--- channel/src/main.rs
+++ channel/src/main.rs
@@ -14,75 +14,69 @@ use std::{
     thread::{self, Thread},
 };
 
-const EMPTY: u8 = 0;
-const WRITING: u8 = 1;
-const READY: u8 = 2;
-const READING: u8 = 3;
-
-pub struct Channel<T> {
-    message: UnsafeCell<MaybeUninit<T>>,
-    state: AtomicU8,
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let a = Arc::new(Channel {
+        message: UnsafeCell::new(MaybeUninit::uninit()),
+        ready: AtomicBool::new(false),
+    });
+    (Sender { channel: a.clone() }, Receiver { channel: a })
 }
 
-unsafe impl<T> Sync for Channel<T> {}
+pub struct Sender<T> {
+    channel: Arc<Channel<T>>,
+}
 
-impl<T> Channel<T> {
-    pub fn new() -> Self {
-        Self {
-            message: UnsafeCell::new(MaybeUninit::uninit()),
-            state: AtomicU8::new(EMPTY),
-        }
+impl<T> Sender<T> {
+    /// このメソッドはパニックしない
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Release);
     }
+}
 
-    pub fn send(&self, message: T) {
-        if self
-            .state
-            .compare_exchange(EMPTY, WRITING, Relaxed, Relaxed)
-            .is_err()
-        {
-            panic!("can't send more than one message!");
-        }
-        unsafe { (*self.message.get()).write(message) };
-        self.state.store(READY, Release);
-    }
+pub struct Receiver<T> {
+    channel: Arc<Channel<T>>,
+}
 
+impl<T> Receiver<T> {
     pub fn is_ready(&self) -> bool {
-        self.state.load(Relaxed) == READY
+        self.channel.ready.load(Relaxed)
     }
 
-    /// メッセージがなければパニックする
-    /// メッセージがすでに読み込まれていてもパニックする
-    pub fn receive(&self) -> T {
-        if self
-            .state
-            .compare_exchange(READY, READING, Acquire, Relaxed)
-            .is_err()
-        {
-            panic!("no message available");
+    pub fn receive(self) -> T {
+        if !self.channel.ready.swap(false, Acquire) {
+            panic!("no message available!");
         }
-        unsafe { (*self.message.get()).assume_init_read() }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
     }
 }
+
+struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
 
 impl<T> Drop for Channel<T> {
     fn drop(&mut self) {
-        if *self.state.get_mut() == READY {
+        if *self.ready.get_mut() {
             unsafe { self.message.get_mut().assume_init_drop() }
         }
     }
 }
 
 fn main() {
-    let channel = Channel::new();
-    let t = thread::current();
     thread::scope(|s| {
-        s.spawn(|| {
-            channel.send("hello world!");
+        let (sender, receiver) = channel();
+        let t = thread::current();
+        s.spawn(move || {
+            sender.send("hello world!");
             t.unpark();
         });
-        while !channel.is_ready() {
+        while !receiver.is_ready() {
             thread::park();
         }
-        assert_eq!(channel.receive(), "hello world!")
+        assert_eq!(receiver.receive(), "hello world!");
     });
 }

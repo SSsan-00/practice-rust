f7ec4eab874efae975c948b35c3b80fc5d7e60da
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-09-13 13:39:21 +0900

    SpinLock完成

--- spin-lock/src/main.rs
+++ spin-lock/src/main.rs
@@ -29,11 +29,11 @@ impl<T> SpinLock<T> {
         }
     }
 
-    pub fn lock<'a>(&'a self) -> &'a mut T {
+    pub fn lock(&self) -> Guard<'_, T> {
         while self.locked.swap(true, Acquire) {
             std::hint::spin_loop();
         }
-        unsafe { &mut *self.value.get() }
+        Guard { lock: self }
     }
 
     pub fn unlock(&self) {
@@ -41,6 +41,43 @@ impl<T> SpinLock<T> {
     }
 }
 
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+unsafe impl<T> Send for Guard<'_, T> where T: Send {}
+unsafe impl<T> Sync for Guard<'_, T> where T: Sync {}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Release);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
 }

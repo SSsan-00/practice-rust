b7fe9257abb2c89b8e682a2e19812a31d79f535c
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-09-13 23:28:46 +0900

    別バージョンのチャネル実装

--- channel/src/main.rs
+++ channel/src/main.rs
@@ -14,31 +14,22 @@ use std::{
     thread::{self, Thread},
 };
 
-pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let a = Arc::new(Channel {
-        message: UnsafeCell::new(MaybeUninit::uninit()),
-        ready: AtomicBool::new(false),
-    });
-    (Sender { channel: a.clone() }, Receiver { channel: a })
+pub struct Sender<'a, T> {
+    channel: &'a Channel<T>,
 }
 
-pub struct Sender<T> {
-    channel: Arc<Channel<T>>,
-}
-
-impl<T> Sender<T> {
-    /// このメソッドはパニックしない
+impl<T> Sender<'_, T> {
     pub fn send(self, message: T) {
         unsafe { (*self.channel.message.get()).write(message) };
         self.channel.ready.store(true, Release);
     }
 }
 
-pub struct Receiver<T> {
-    channel: Arc<Channel<T>>,
+pub struct Receiver<'a, T> {
+    channel: &'a Channel<T>,
 }
 
-impl<T> Receiver<T> {
+impl<T> Receiver<'_, T> {
     pub fn is_ready(&self) -> bool {
         self.channel.ready.load(Relaxed)
     }
@@ -51,9 +42,23 @@ impl<T> Receiver<T> {
     }
 }
 
-struct Channel<T> {
+pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
     ready: AtomicBool,
+}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
+        *self = Self::new();
+        (Sender { channel: self }, Receiver { channel: self })
+    }
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
@@ -67,8 +72,9 @@ impl<T> Drop for Channel<T> {
 }
 
 fn main() {
+    let mut channel = Channel::new();
     thread::scope(|s| {
-        let (sender, receiver) = channel();
+        let (sender, receiver) = channel.split();
         let t = thread::current();
         s.spawn(move || {
             sender.send("hello world!");

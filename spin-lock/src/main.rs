b2a8ed2737324d784081669092c654a379cc0b2f
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-09-13 13:03:52 +0900

    スピンロック: 最小実装

--- spin-lock/src/main.rs
+++ spin-lock/src/main.rs
@@ -1,3 +1,41 @@
+#[allow(unused)]
+use std::{
+    cell::{Cell, RefCell, UnsafeCell},
+    collections::VecDeque,
+    marker::PhantomData,
+    mem::{ManuallyDrop, MaybeUninit},
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+    rc::Rc,
+    sync::{
+        atomic::{Ordering::*, *},
+        *,
+    },
+    thread::{self, Thread},
+};
+
+pub struct SpinLock {
+    locked: AtomicBool,
+}
+
+impl SpinLock {
+    pub const fn new() -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+        }
+    }
+
+    pub fn lock(&self) {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+    }
+
+    pub fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }

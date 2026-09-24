7f012951efa8b6cfe439de37f60a6d300450b96d
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-09-25 00:07:26 +0900

    設定・削除・取得機能追加

--- mini-db/src/main.rs
+++ mini-db/src/main.rs
@@ -14,7 +14,7 @@ impl Database {
         }
     }
 
-    // keyとvalueの所有権をDBへ私て保存する
+    // keyとvalueの所有権をDBへ渡して保存する
     fn set(&mut self, key: String, value: String) {
         self.data.insert(key, value);
     }
@@ -24,11 +24,47 @@ impl Database {
     fn get(&self, key: &str) -> Option<&str> {
         self.data.get(key).map(String::as_str)
     }
+
+    // 対応する値を削除する
+    // 削除する値を返し、存在しなければNoneを返す
+    fn remove(&mut self, key: &str) -> Option<String> {
+        self.data.remove(key)
+    }
+}
+
+enum Command {
+    Exit,
+    Set { key: String, value: String },
+    Get { key: String },
+    Remove { key: String },
+    Unknown(String),
+}
+
+fn parse_command(input: &str) -> Command {
+    let words: Vec<&str> = input.split_whitespace().collect();
+
+    match words.as_slice() {
+        [".exit"] => Command::Exit,
+        ["set", key, value] => Command::Set {
+            key: key.to_string(),
+            value: value.to_string(),
+        },
+        ["get", key] => Command::Get {
+            key: key.to_string(),
+        },
+        ["remove", key] => Command::Remove {
+            key: key.to_string(),
+        },
+        _ => Command::Unknown(input.to_string()),
+    }
 }
 
 fn main() -> io::Result<()> {
     // 入力を受け取る
     let mut input = String::new();
+
+    // DBを作成する
+    let mut db = Database::new();
 
     loop {
         // 前回の入力を空にする
@@ -45,11 +81,26 @@ fn main() -> io::Result<()> {
             break;
         }
 
-        // Enterによる改行を取り除いてから判定する
-        match input.trim() {
-            ".exit" => break,
-            // .exit以外の入力をcommandという変数で受け取る
-            command => println!("Unrecognized command '{command}'"),
+        // 入力で受け取った文字列をCommandに変換する
+        let command = parse_command(input.trim());
+
+        match command {
+            Command::Exit => break,
+            Command::Set { key, value } => {
+                db.set(key, value);
+                println!("OK")
+            }
+            Command::Get { key } => match db.get(&key) {
+                Some(value) => println!("{value}"),
+                None => println!("(nil)"),
+            },
+            Command::Remove { key } => match db.remove(&key) {
+                Some(_) => println!("OK"),
+                None => println!("(nil)"),
+            },
+            Command::Unknown(input) => {
+                println!("Unrecognized command '{input}'");
+            }
         }
     }
 
@@ -67,5 +118,15 @@ mod tests {
         db.set(String::from("name"), String::from("Taro"));
 
         assert_eq!(db.get("name"), Some("Taro"));
+    }
+
+    #[test]
+    fn removes_a_value() {
+        let mut db = Database::new();
+
+        db.set(String::from("name"), String::from("Taro"));
+
+        assert_eq!(db.remove("name"), Some(String::from("Taro")));
+        assert_eq!(db.get("name"), None);
     }
 }
